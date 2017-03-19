fc0a5d78540e4db7a6a9fb7f405961156f945c89
John Berry <jarcane@users.noreply.github.com>
2017-03-19 10:39:17 +0200

    lang: jsonic

---
+++ jsonic/jsonic-test.rkt
@@ -0,0 +1,13 @@
+#lang jsonic
+// a line comment
+[
+    @$ 'null $@,
+    @$ #f $@,
+    @$ #t $@,
+    @$ (* 6 7) $@,
+    @$ "string" $@,
+    @$ (list "array" "of" "strings") $@,
+    @$ (hash 'key-1 42
+             'key-2 "value"
+             'key-3 (hash 'subkey 21)) $@
+]
